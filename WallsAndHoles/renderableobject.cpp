fbdb27917153d7d3c4e4c4aa74614f4c096e8435
timoffex <timoffex@gmail.com>
2018-01-26 23:33:20 -0800

    Added normals & materials to RenderableObject
    RenderableObject now has material and normal information for every
    vertex.

--- WallsAndHoles/renderableobject.cpp
+++ WallsAndHoles/renderableobject.cpp
@@ -10,16 +10,79 @@ RenderableObject::RenderableObject() {
 
 RenderableObject::RenderableObject(QVector<QVector3D> vertices)
     : mVertexPositions(vertices),
+      mVertexMaterials(vertices.size(), MeshMaterial(1, 1, 1, 1)),
       mTriangleIndices(vertices.size(), 0) {
 
-    assert( vertices.size() % 3 == 0 );
+    Q_ASSERT( vertices.size() % 3 == 0 );
 
     // Sets mTriangleIndices to {0, 1, 2, ..., vertices.size()}
     std::iota(mTriangleIndices.begin(), mTriangleIndices.end(), 0);
+
+    generateNormals();
 }
 
 RenderableObject::RenderableObject(QVector<QVector3D> vertices, QVector<unsigned int> indices)
     : mVertexPositions(vertices),
-        mTriangleIndices(indices) {
-    assert( indices.size() % 3 == 0 );
+      mVertexMaterials(vertices.size(), MeshMaterial(1, 1, 1, 1)),
+      mTriangleIndices(indices) {
+
+    Q_ASSERT( indices.size() % 3 == 0 );
+
+    generateNormals();
+}
+
+
+RenderableObject::RenderableObject(QVector<QVector3D> vertices, QVector<QVector3D> normals, QVector<unsigned int> indices)
+    : mVertexPositions(vertices),
+      mVertexNormals(normals),
+      mVertexMaterials(vertices.size(), MeshMaterial(1, 1, 1, 1)),
+      mTriangleIndices(indices) {
+
+    Q_ASSERT( indices.size() % 3 == 0 );
+}
+
+RenderableObject::RenderableObject(QVector<QVector3D> vertices, QVector<QVector3D> normals, QVector<MeshMaterial> materials, QVector<unsigned int> indices)
+    : mVertexPositions(vertices),
+      mVertexNormals(normals),
+      mVertexMaterials(materials),
+      mTriangleIndices(indices) {
+
+    Q_ASSERT( indices.size() % 3 == 0 );
+}
+
+
+
+void RenderableObject::generateNormals() {
+    // The normal of a vertex will be the average of the geometric normal vectors of the triangles incident on it.
+
+    mVertexNormals = QVector<QVector3D>(mVertexPositions.size(), QVector3D(0, 0, 0));
+
+    // 1) Accumulate.
+    for (int i = 0; i < mTriangleIndices.size(); i += 3) {
+        QVector3D v1 = mVertexPositions[i*3 + 0],
+                v2 = mVertexPositions[i*3 + 1],
+                v3 = mVertexPositions[i*3 + 2];
+
+        QVector3D diff1 = v2 - v1,
+                diff2 = v3 - v1;
+
+        QVector3D normal = QVector3D::normal(diff1, diff2);
+
+        mVertexNormals[i*3 + 0] += normal;
+        mVertexNormals[i*3 + 1] += normal;
+        mVertexNormals[i*3 + 2] += normal;
+    }
+
+    // 2) Normalize.
+    for (int i = 0; i < mVertexNormals.size(); ++i) {
+        QVector3D normal = mVertexNormals[i];
+
+        // If this assert fails, that means that the sum of normal vectors for a vertex
+        // is 0. This is almost certainly because triangle indices are not specified
+        // in a consistent order -- see the comment at the declaration of generateNormals()
+        // in renderableobject.h
+        Q_ASSERT( normal.lengthSquared() != 0 );
+
+        mVertexNormals[i] = normal.normalized();
+    }
 }
--- WallsAndHoles/renderableobject.h
+++ WallsAndHoles/renderableobject.h
@@ -5,6 +5,8 @@
 #include <QVector2D>
 #include <QVector3D>
 
+#include "meshmaterial.h"
+
 
 /**
  * @brief The RenderableObject class
@@ -17,15 +19,30 @@ public:
     RenderableObject();
     RenderableObject(QVector<QVector3D> vertices);
     RenderableObject(QVector<QVector3D> vertices, QVector<unsigned int> indices);
+    RenderableObject(QVector<QVector3D> vertices, QVector<QVector3D> normals, QVector<unsigned int> indices);
+    RenderableObject(QVector<QVector3D> vertices, QVector<QVector3D> normals, QVector<MeshMaterial> materials, QVector<unsigned int> indices);
 
-    const QVector<QVector3D>& getVertexData() const { return mVertexPositions; }
+    const QVector<QVector3D>& getVertexData() const { return mVertexPositions; }    
+    const QVector<QVector3D>& getVertexNormals() const { return mVertexNormals; }
+    const QVector<MeshMaterial>& getVertexMaterials() const { return mVertexMaterials; }
+
     const QVector<unsigned int>& getTriangleIndices() const { return mTriangleIndices; }
 
     size_t getNumVertices() const { return mVertexPositions.size(); }
 
 protected:
 
+    /**
+     * @brief Generate normal vectors automatically, assuming mVertexPositions
+     * and mTriangleIndices are set. This assumes that triangle indices are
+     * specified in counterclockwise order, that is if the triangle is rendered
+     * facing the camera, then the indices enumerate the vertices counterclockwise.
+     */
+    void generateNormals();
+
     QVector<QVector3D> mVertexPositions;
+    QVector<QVector3D> mVertexNormals;
+    QVector<MeshMaterial> mVertexMaterials;
 
     /**
      * @brief mTriangleIndices Stores a list of indices to the mVertex_ arrays above.
