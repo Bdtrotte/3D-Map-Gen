793aed042d79be4f0c40f542015fb2590a40edb9
timoffex <timoffex@gmail.com>
2018-01-28 10:22:50 -0800

    Cleaned up MainWindow and got rid of .orig files
    MainWindow still has the export/import actions. It may be worth moving
    them elsewhere.

--- WallsAndHoles/mainwindow.cpp
+++ WallsAndHoles/mainwindow.cpp
@@ -2,9 +2,7 @@
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "mapview.h"
-#include "QDebug"
-#include "meshviewcontainer.h"
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -18,17 +16,6 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::openMeshView() {
-    if (mMeshViewContainer == nullptr) {
-        mMeshViewContainer = new MeshViewContainer();
-        mMeshViewContainer->show();
-    }
-}
-
-void MainWindow::on_pushButton_clicked() {
-    openMeshView();
-}
-
 
 void MainWindow::on_actionopen_triggered()
 {
@@ -38,30 +25,36 @@ void MainWindow::on_actionsave_triggered()
 {
 }
 
-void MainWindow::on_actionexport_triggered()
-{   mMeshViewContainer = findChild<MeshViewContainer *>();
-    if (mMeshViewContainer == nullptr) {
+void MainWindow::on_actionexport_triggered() {
+    MeshViewContainer *meshViewContainer = findChild<MeshViewContainer *>();
+
+    if (meshViewContainer == nullptr) {
         QMessageBox messageBox;
         messageBox.critical(0,"Error","MeshView doesn't exist!");
         messageBox.setFixedSize(500,200);
         return;
     }
+
     QString fileName = QFileDialog::getSaveFileName(this,
         tr("Export OBJ"), "/home", tr("Export Files (*.obj)"));
+
     if(!fileName.isEmpty())
-        mMeshViewContainer->saveMesh(fileName);
+        meshViewContainer->saveMesh(fileName);
 }
 
-void MainWindow::on_actionimport_triggered(){
-    mMeshViewContainer = findChild<MeshViewContainer *>();
-    if (mMeshViewContainer == nullptr) {
+void MainWindow::on_actionimport_triggered() {
+    MeshViewContainer *meshViewContainer = findChild<MeshViewContainer *>();
+
+    if (meshViewContainer == nullptr) {
         QMessageBox messageBox;
         messageBox.critical(0,"Error","MeshView doesn't exist!");
         messageBox.setFixedSize(500,200);
         return;
     }
+
     QString fileName = QFileDialog::getOpenFileName(this,
         tr("import OBJ"), "/home", tr("import Files (*.obj)"));
+
     if(!fileName.isEmpty())
-        mMeshViewContainer->loadMesh(fileName);
+        meshViewContainer->loadMesh(fileName);
 }
